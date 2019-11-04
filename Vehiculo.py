8899e2756d8d89709fe58e43eb6a8fbb87af77e7
jesusgamtello <56297001+jesusgamtello@users.noreply.github.com>
2019-11-05 00:07:42 +0100

    Clase vehiculo

---
+++ Vehiculo.py
@@ -0,0 +1,29 @@
+class Vehiculo(object):
+    #matrícula, kilometraje, precio por día,modelo
+    def __init__(self,matricula,kilometraje,precio_dia,modelo):
+        self.matricula=matricula
+        self.kilometraje=kilometraje
+        self.precio_dia=precio_dia
+        self.modelo=modelo
+    
+
+    def get_matricula(self):
+        return self.matricula
+    
+    def get_kilometraje(self):
+        return self.kilometraje
+
+    def get_precio_dia(self):
+        return self.precio_dia
+    
+    def get_modelo(self):
+        return self.modelo
+    
+    def set_matricula(self,valor):
+        self.matricula=valor
+
+    def set_kilometraje(self,valor):
+        self.kilometraje=valor
+    def set_precio_dia(self,valor):
+        self.precio_dia=valor
+
