4d16b69e7b38e1c9fc54c1b58c9dc93875f2320e
jesusgamtello <56297001+jesusgamtello@users.noreply.github.com>
2019-11-05 22:31:04 +0100

    cambios minimos en el codigo

--- __main__.py
+++ __main__.py
@@ -1,5 +1,4 @@
-import os 
-import argparse
+import os
 from Concesionario import Concesionario
 from leer_ficheros import Leer_ficheros
 
@@ -10,8 +9,8 @@ coches=p.crear_objeto_vehiculo()
 concesionario=Concesionario(coches,clientes)
 
 def menu():
-    
-    os.system('clear')
+
+    os.system('cls') #seria clear en caso de ejecutar en linux
     print('Bienvenido al menú de nuestra aplicacion\n')
     print('-------------------------------------------\n')
     print('Seleccione una opcion:\n')
@@ -22,24 +21,24 @@ def menu():
     print('5.Saber si un cliente es VIP\n')
     print('6.Consultar descuentos de un cliente\n')
     print('7.Salir\n')
-    
+
 
 while True:
     menu()
     opcion=int(input("La opcion del menu elegida es >>"))
 
     if opcion==1:
-        print("leyendo vehiculos disponibles...\n")        
-        if clientes!=None:
+        print("leyendo vehiculos disponibles...\n")
+        if clientes != None:
             concesionario.consultar_vehiculos()
-                
+
         input('\nTodos los clientes mostrados\nPulse una tecla para continuar')
     elif opcion==2:
         print("")
         input('alquilar un vehiculo\nPulse una tecla para continuar')
     elif opcion==3:
         DNI=str(input("Introduzca el DNI >>\n"))
-        if concesionario.comprobar_cliente(DNI)==True:
+        if concesionario.comprobar_cliente(DNI):
             concesionario.consultar_reservas(DNI)
         else:
             print("el cliente no se encuentra en la base de datos")
@@ -49,19 +48,19 @@ while True:
         input('mostrar contrato\nPulse una tecla para continuar')
     elif opcion==5:
         DNI=str(input("Introduzca el DNI >>\n"))
-        if concesionario.comprobar_cliente(DNI)==True:
-            if concesionario.consultar_vip(DNI)==True:
+        if concesionario.comprobar_cliente(DNI):
+            if concesionario.consultar_vip(DNI):
                 print("el cliente es VIP")
             else:
                 print("El cliente NO es VIP")
-        else: 
+        else:
             print("el cliente no existe en la base de datos")
         input('\nPulse una tecla para continuar')
     elif opcion==6:
         DNI=str(input("Introduzca el DNI >>\n"))
-        if concesionario.comprobar_cliente(DNI)==True:
-            print("El descuento del cliente es de ",concesionario.devolver_cliente(DNI).get_descuento())
-        else: 
+        if concesionario.comprobar_cliente(DNI):
+            print("El descuento del cliente es de ", concesionario.devolver_cliente(DNI).get_descuento())
+        else:
             print("el cliente no existe en la base de datos")
         input('\nPulse una tecla para continuar')
     elif opcion==7:
